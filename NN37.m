471935478ad53e27ace4c7b8f898b143cf611fae
Kleomenis <kleomenischatzigeorgiou@gmail.com>
2019-01-30 02:54:14 +0100

    Update NN37.m

--- NN37.m
+++ NN37.m
@@ -1,46 +1,72 @@
-X = csvread('/home/guillem/loc/Assoc37.csv')
-y = csvread('/home/guillem/loc/Rew37binary.csv')
 
-% This script assumes these variables are defined:
-%
-%   X - input data.
-%   y - target data.
+%creates a struct with the name of the training algorithms
+nd=25
+field = 'f'
+value = 'trainlm'
+field2 = 'f2'
+field3 = 'f3'
+value2 = 'trainbr'
+value3 = 'trainscg'
+list = struct (field, value,field2,value2, field3, value3)
 
-x = X';
-t = y';
+fns = fieldnames(list);
 
-% Choose a Training Function
-% For a list of all training functions type: help nntrain
-% 'trainlm' is usually fastest.
-% 'trainbr' takes longer but may be better for challenging problems.
-% 'trainscg' uses less memory. Suitable in low memory situations.
-trainFcn = 'trainscg';  % Levenberg-Marquardt backpropagation.
+%for loop to try different number of nodes each time (cross validation)
+%variable cvnodes is the number of time we want to increase the number of nodes by 5
+for cvnodes = 1:10
 
-% Create a Fitting Network
-hiddenLayerSize = 25;
-net = fitnet(hiddenLayerSize,trainFcn);
+    %read input and output from CSV
+    X = csvread('/home/guillem/loc/Assoc37.csv')
+    y = csvread('/home/guillem/loc/Rew37binary.csv')
 
-% Setup Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 100/100;
-net.divideParam.valRatio = 0/100;
-net.divideParam.testRatio = 0/100;
+    %formating input and target
+    x = X';
+    t = y';
+    
+    % increase the number of nodes by 5
+    nd = nd + 5 
+    
+    %for loop to try different training algorithms for each node value
+    for lam = 1:3 
+        
+        %crossvalidation for the training algorithm
+        trainFcn = list.(fns{lam}) ;  
+        name = list.(fns{lam})
 
-% Train the Network
-[net,tr] = train(net,x,t);
+        % Create a Fitting Networt
+        hiddenLayerSize = nd;
+        net = fitnet(hiddenLayerSize,trainFcn);
 
-% Test the Network
-y = net(x);
-e = gsubtract(t,y);
-performance = perform(net,t,y)
+        % Setup Division of Data for Training, Validation, Testing
+        net.divideParam.trainRatio = 100/100;
+        net.divideParam.valRatio = 0/100;
+        net.divideParam.testRatio = 0/100;
 
-% View the Network
-view(net)
+        % Train the Network
+        [net,tr] = train(net,x,t);
 
-% Plots
-% Uncomment these lines to enable various plots.
-figure, plotperform(tr)
-%figure, plottrainstate(tr)
-%figure, ploterrhist(e)
-figure, plotregression(t,y)
-%figure, plotfit(net,x,t)
+        % Test the Network
+        y = net(x);
+        e = gsubtract(t,y);
+        performance = perform(net,t,y)
+        
+        %Writes the results to a CSV file
+        results = [performance,hiddenLayerSize,lam]
+        dlmwrite ('/home/guillem/loc/results37.csv', results, 'delimiter',',', '-append');
+        % View the Network
+        view(net)
 
+        % Plots
+        % Uncomment these lines to enable various plots.
+        %figure, plotperform(tr)
+        %figure, plottrainstate(tr)
+        %figure, ploterrhist(e)
+        %figure, plotregression(t,y)
+        %figure, plotfit(net,x,t)
+        
+    end
+   
+    clear x
+    clear X
+    clear y
+end
